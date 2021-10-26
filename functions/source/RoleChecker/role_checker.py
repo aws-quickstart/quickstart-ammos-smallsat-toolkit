d948ac3919c05750c28f3f72b383e0d72d15e762
Michael Fedell <michaelfedell14@gmail.com>
2021-10-26 12:59:29 -0700

    Reference roles in nested stacks and fix bugs

--- functions/source/RoleChecker/role_checker.py
+++ functions/source/RoleChecker/role_checker.py
@@ -13,27 +13,33 @@ logger.setLevel(os.getenv("LOG_LEVEL", "DEBUG"))
 def lambda_handler(event, context):
     try:
         logger.debug("Received event: {}".format(json.dumps(event)))
-        result = cfnresponse.FAILED
+        result = cfnresponse.SUCCESS
+        reason = None
         client = boto3.client("iam")
 
         # Pull identifiers from the request (passes as Properties in the custom resource)
         role_names = event["ResourceProperties"].get("RoleNames", [])
         role_arns = {}
+        missing_roles = []
 
         if event["RequestType"] in ["Create", "Update"]:
             for name in role_names:
+                key = name.split("-")[-1]  # Strip the leading ProjectName from role name
                 try:
                     logger.debug(f"Checking Account Roles for {name}")
                     arn = client.get_role(RoleName=name)["Role"]["Arn"]
                     logger.debug(f"Role already exists: {arn}")
-                    role_arns[name] = arn
+                    role_arns[key] = arn
                 except botocore.exceptions.ClientError as e:
                     if e.response["Error"]["Code"] == "NoSuchEntity":
-                        logger.debug(f"{name} Role does not exist")
-                        role_arns[name] = "NA"
+                        # TODO: Do we really want to fail here or just mark the missing role as NA
+                        logger.error(f"{name} Role does not exist")
+                        # role_arns[key] = "NA"
+                        result = cfnresponse.FAILED
+                        missing_roles.append(name)
                     else:
                         logger.error("Uncaught boto exception", e)
-            result = cfnresponse.SUCCESS
+                        result = cfnresponse.FAILED
         elif event["RequestType"] == "Delete":
             logger.info("Delete request - NOOP")
             result = cfnresponse.SUCCESS
@@ -41,9 +47,12 @@ def lambda_handler(event, context):
     except Exception as e:
         logger.error("Error: {}".format(e))
         result = cfnresponse.FAILED
+
     responseData = role_arns
-    logger.info("Returning response of: {}, with result of: {}".format(
-        result, responseData))
+    if result == cfnresponse.FAILED:
+        reason = ("Required roles were not found in account; please use or refer to the ast-iam-role template for a "
+                  "list of required roles. The following roles were not found: " + ", ".join(missing_roles))
+    logger.info("Returning response of: {}, with result of: {}".format(result, responseData))
     sys.stdout.flush()
     # https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/cfn-lambda-function-code-cfnresponsemodule.html
-    cfnresponse.send(event, context, result, responseData)
+    cfnresponse.send(event, context, result, responseData, reason=reason)
