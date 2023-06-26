dc645291264866addbe6a9261c4db218c65043df
Michael Fedell <mfedell@jpl.nasa.gov>
2023-06-26 10:13:04 -0700

    fix: remove dead code and stale references

--- functions/source/BootstrapAerie/bootstrap_aerie.py
+++ functions/source/BootstrapAerie/bootstrap_aerie.py
@@ -25,18 +25,6 @@ class EFS:
 @helper.create
 def clone_deployment(event, _):
     aerie_version = event["ResourceProperties"].get("AerieVersion", AERIE_VERSION)
-    # Unsure why this is here - don't think we need to clean out /tmp on every invocation
-    # if os.listdir("/tmp"):
-    #     folder = "/tmp"
-    #     for filename in os.listdir(folder):
-    #         file_path = os.path.join(folder, filename)
-    #         try:
-    #             if os.path.isfile(file_path) or os.path.islink(file_path):
-    #                 os.unlink(file_path)
-    #             elif os.path.isdir(file_path):
-    #                 shutil.rmtree(file_path)
-    #         except Exception as e:
-    #             print("Failed to delete %s. Reason: %s" % (file_path, e))
 
     url = f"https://github.com/NASA-AMMOS/aerie/releases/download/{aerie_version}/Deployment.zip"
     filehandle = urllib.request.urlopen(url)
