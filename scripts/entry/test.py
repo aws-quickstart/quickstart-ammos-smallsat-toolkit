63d09a94239629c7619455b1d67224c6df827d58
James Jen <james.j.jen@jpl.nasa.giv>
2023-07-13 12:52:17 -0700

    removed extraneous files

--- scripts/entry/test.py
+++
@@ -1,9 +0,0 @@
-import app
-
-config_entry_stack = app.ConfigEntryStack.new(
-    'jjen-configurator-test',
-    'data/real.config.json',
-    'foobar.yaml',
-)
-
-config_entry_stack.get_entry_yaml()
