e9e12bb98ce9a9f4f8607835ef4f2553f2a161fc
James Jen <james.j.jen@jpl.nasa.giv>
2023-07-31 04:09:56 -0700

    updaed entry stack generation script to update main stack filename

--- scripts/entry/app.py
+++ scripts/entry/app.py
@@ -3,6 +3,7 @@ from typing import Dict
 import cfn_flip
 import json
 import subprocess
+import pathlib
 
 class Utils:
     @staticmethod
@@ -29,21 +30,24 @@ class EntryTemplateFactory:
         main_dict = Utils.get_dict_from_template(main_filepath)
         boilerplate_dict = Utils.get_dict_from_template(boilerplate_filepath)
 
+
         params = {
             'boilerplate': boilerplate_dict,
             'main': main_dict,
         }
 
         factory = EntryTemplateFactory(**params)
-        factory._update_entry()
+        factory._update_entry(main_filepath)
         return factory
 
-    def get_entry_template(self,filename='entry'):
+    def get_entry_template(self,filepath='output/entry.yaml'):
         entry_dict = self.entry
-        with open(f"output/{filename}.json",'w') as f:
+        filename = pathlib.Path(filepath).stem
+        path = pathlib.Path(filepath).parent
+        with open(f"{path}/{filename}.json",'w') as f:
             f.write(json.dumps(entry_dict))
 
-        command = f"cfn-flip output/{filename}.json output/{filename}.yaml"
+        command = f"cfn-flip {path}/{filename}.json output/{filename}.yaml"
         subprocess.run(command, shell=True)
 
 
@@ -52,15 +56,30 @@ class EntryTemplateFactory:
     ##  Methods used in _update_entry()  ##
     #######################################
 
-    def _update_entry(self):
+    def _update_entry(self, main_filepath):
         entry = self.boilerplate.copy()
-
+        
         # Main Stack Parameters
         main_params = self._get_entry_main_params()
         entry['Resources']['MainStack']['Properties']['Parameters'] = main_params
 
-        self.entry= entry
+        # Updating Main Stack URL filename
+        self._update_main_stack_filename(main_filepath, entry)
 
+        # Setting entry
+        self.entry = entry
+
+
+    def _update_main_stack_filename(self, main_template_filepath, entry):
+        main_template_filename = pathlib.Path(main_template_filepath).name
+        main_stack_url_sub_list = entry['Resources']['MainStack']['Properties']['TemplateURL']['Fn::Sub']
+        
+        url = main_stack_url_sub_list[0]
+        url_split = url.split('/')
+        url_split[-1] = main_template_filename
+        url = '/'.join(url_split)
+        main_stack_url_sub_list[0] = url
+        
 
     def _get_entry_main_params(self, custom_resource_name = 'Config'):
         cr = custom_resource_name
