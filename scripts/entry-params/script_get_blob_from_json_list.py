33489e44a0e69c448bf62ed287238d1f495a2370
James Jen <james.j.jen@jpl.nasa.giv>
2023-08-03 13:11:23 -0700

    some changes I no longer remember why I made

---
+++ scripts/entry-params/script_get_blob_from_json_list.py
@@ -0,0 +1,9 @@
+import json
+from params import Params
+
+with open('main-entry.json','r') as f:
+    params_json_list = json.load(f)
+
+params = Params.load_from_list(params_json_list)
+blob = params.get_query_string()
+print(blob)
